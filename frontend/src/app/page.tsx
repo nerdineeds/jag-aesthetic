4234367f5aa55a088202154988978035dfe7e223
nerdineeds <NERDINEEDS@GMAIL.COM>
2024-07-17 19:10:11 -0400

    updated loading from data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,9 @@
+'use client';
+import { useState, useEffect } from 'react';
 import { flattenAttributes } from '@/utils/utils';
 import { getStrapiData } from '../../data/strapi/getStrapiData';
 import { HeroSection } from '../components/custom/HeroSection';
 import ImageHero from '../components/ImageHero/ImageHero';
-import Comparison from '../components/Comparison/Comparison';
 import Packages from '../components/Packages/Packages';
 
 function blockRenderer(block: any) {
@@ -18,14 +19,30 @@ function blockRenderer(block: any) {
   }
 }
 
-export default async function Home() {
-  const strapiData = await getStrapiData('home-page');
-  const data = flattenAttributes(strapiData);
-  const { blocks } = data;
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [blocks, setBlocks] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-  if (!blocks) return <p>No sections found</p>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const strapiData = await getStrapiData('home-page');
+        const data = flattenAttributes(strapiData);
+        setBlocks(data.blocks || []);
+      } catch (err) {
+        setError('Failed to fetch data');
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  console.log(blocks);
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  if (!blocks.length) return <p>No sections found</p>;
 
   return (
     <div className="relative flex flex-col lg:gap-y-12 items-center">
